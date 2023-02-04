efc8777bac07714c783d807b1f33da2c7830b831
Alex Baldwin <alexbaldwin2876@gmail.com>
2023-02-04 18:05:56 +0000

    Improvments to build script and PWM loop

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export PICO_SDK_PATH=/home/alex/software/pico-sdk
+rm -r build
+mkdir build
 cd build
 cmake ..
 make -j4
--- main.c
+++ main.c
@@ -65,9 +65,6 @@ void motor_output_step_5() {
 
 void setup_gpio() {
     adc_init();
-    //adc_gpio_init(26);
-    //adc_gpio_init(27);
-    //adc_gpio_init(28);
     adc_gpio_init(29);
     
     gpio_init(MOTOR_SENSE_U);
@@ -156,16 +153,9 @@ double rotor_phase_position = 0; // In rad
 double rotor_average_speed = 0; // In rads-1
 
 void check_rotor_position() {
-    //adc_select_input(0);
-    //uint16_t result_u = adc_read();
-    //adc_select_input(1);
-    //uint16_t result_v = adc_read();
-    //adc_select_input(2);
-    //uint16_t result_w = adc_read();
-    
-    bool parsed_u = gpio_get(MOTOR_SENSE_U);//result_u > 1024;
-    bool parsed_v = gpio_get(MOTOR_SENSE_V);//result_v > 1024;
-    bool parsed_w = gpio_get(MOTOR_SENSE_W);//result_w > 1024;
+    bool parsed_u = gpio_get(MOTOR_SENSE_U);
+    bool parsed_v = gpio_get(MOTOR_SENSE_V);
+    bool parsed_w = gpio_get(MOTOR_SENSE_W);
     
     int8_t rotor_proposed_step = -1;
     
@@ -193,7 +183,13 @@ void check_rotor_position() {
     if (rotor_proposed_step != -1) {
         if (rotor_current_step != rotor_proposed_step) {
             if (rotor_proposed_step != rotor_last_step) { // Check we're not just oscillating
-                steps_jumped = abs(rotor_proposed_step - rotor_current_step);
+                steps_jumped = ((rotor_proposed_step - rotor_current_step) + 6) % 6;
+                if (steps_jumped >= 3) {
+                    steps_jumped = 6 - steps_jumped;
+                }
+                if (steps_jumped < 0) {
+                    steps_jumped = 0;
+                }
                 rotor_last_step = rotor_current_step;
                 rotor_current_step = rotor_proposed_step;
                 last_step_time = time_since_last_step;
@@ -203,6 +199,8 @@ void check_rotor_position() {
                 }
                 
                 time_since_last_step = 0;
+                
+                
             }
         }
         if (time_since_last_step > 1000000.0) {
@@ -228,17 +226,14 @@ void estimate_rotor_position() {
     }
 }
 
-uint64_t time_since_last_report = 0;
+
 // CONTROL METHODDS GO HERE
 
 double pwm_control_frequency = 175;
-double pwm_control_executions_per_phase_cycle = 16;
-uint64_t pwm_control_motor_execution_count = 0;
 
 void pwm_control_loop() {
-    pwm_control_motor_execution_count++;
     check_rotor_position();
-    estimate_rotor_position();
+    //estimate_rotor_position();
     
     uint8_t rotor_pulse_step = 0;
     
@@ -247,28 +242,52 @@ void pwm_control_loop() {
     
     rotor_pulse_step = rotor_current_step;
     rotor_pulse_step += 2;
-    
     rotor_pulse_step = rotor_pulse_step % 6;
     
-    if (motor_duty_cycle == 1.0) {
+    uint64_t step_time = (uint64_t) ((1.0 / pwm_control_frequency) * 1000000);
+    uint64_t on_time = step_time * motor_duty_cycle;
+    uint64_t sleep_time = step_time - on_time;
+    
+    if (on_time > 0) {
         motor_output_on(rotor_pulse_step);
-        gpio_put(LED_A_PIN, 1);
-        gpio_put(LED_B_PIN, 0);
-    } else {
-        if (pwm_control_motor_execution_count < (pwm_control_executions_per_phase_cycle * motor_duty_cycle)) {
-            motor_output_on(rotor_pulse_step);
-            gpio_put(LED_A_PIN, 1);
-            gpio_put(LED_B_PIN, 0);
-        } else {
-            motor_output_off();
-            gpio_put(LED_A_PIN, 0);
-            gpio_put(LED_B_PIN, 1);
-        }
+        sleep_us(on_time);
     }
-    if (pwm_control_motor_execution_count >  pwm_control_executions_per_phase_cycle) {
-        pwm_control_motor_execution_count = 0;
+    motor_output_off();
+    sleep_us(sleep_time);
+    
+    time_since_last_step += step_time;
+}
+
+bool r2sw = false;
+
+uint64_t steps_since_last_report = 0;
+
+int main() {
+    setup_gpio();
+    stdio_init_all();
+    while (true) {
+        adc_select_input(3);
+        uint16_t throttle = adc_read();
+        
+        motor_duty_cycle = ((throttle - 1380) / 2808.0);
+        if (motor_duty_cycle > 0.9) {
+            motor_duty_cycle = 0.9;
+        }
+        
+        if (motor_duty_cycle < 0.0) {
+            motor_duty_cycle = 0.0;
+        }
+        
+        steps_since_last_report++;
+        if (steps_since_last_report > 100) {
+            //check_rotor_position();
+            //estimate_rotor_position();
+            printf("%3.2f Hz; %3.2f rads-1; %d Hz PWM; S %d; THR %1.2f\n", rotor_phase_frequency, rotor_average_speed, (int) pwm_control_frequency, rotor_current_step, motor_duty_cycle);
+            steps_since_last_report = 0;
+        }
+        
         pwm_control_frequency = 175;
-        double rotor_output_multiplier = 45.0;
+        double rotor_output_multiplier = 15;
         if (rotor_phase_frequency > (0.6 * rotor_output_multiplier)) {
             pwm_control_frequency = 197;
         }
@@ -278,11 +297,20 @@ void pwm_control_loop() {
         if (rotor_phase_frequency > (1.8 * rotor_output_multiplier)) {
             pwm_control_frequency = 234;
         }
-        if (rotor_phase_frequency > (2.6 * rotor_output_multiplier)) {
+        if (rotor_phase_frequency > (2.4 * rotor_output_multiplier)) {
             pwm_control_frequency = 262;
         }
-        if (rotor_phase_frequency > (3.2 * rotor_output_multiplier)) {
+        if (rotor_phase_frequency > (3.0 * rotor_output_multiplier)) {
             pwm_control_frequency = 294;
+        }
+        if (rotor_phase_frequency > (3.6 * rotor_output_multiplier)) {
+            pwm_control_frequency = 312;
+        }
+        if (rotor_phase_frequency > (4.2 * rotor_output_multiplier)) {
+            pwm_control_frequency = 350;
+        }
+        if (rotor_phase_frequency > (4.8 * rotor_output_multiplier)) {
+            pwm_control_frequency = 400;
         }
         /*
         175Hz       0.0Hz
@@ -294,82 +322,6 @@ void pwm_control_loop() {
         312Hz       3.8Hz
         350Hz       4.4Hz
         400Hz       5.0Hz
-        */
-    }
-    uint64_t sleep_time = (uint64_t) ((1.0 / (pwm_control_frequency * pwm_control_executions_per_phase_cycle)) * 1000000);
-    time_since_last_step += sleep_time;
-    time_since_last_report += sleep_time;
-    sleep_us(sleep_time);
-}
-
-bool r2sw = false;
-
-int main() {
-    setup_gpio();
-    stdio_init_all();
-    while (true) {
-        adc_select_input(3);
-        uint16_t throttle = adc_read();
-        //adc_select_input(0);
-        //uint16_t result_u = adc_read();
-        //adc_select_input(1);
-        //uint16_t result_v = adc_read();
-        //adc_select_input(2);
-        //uint16_t result_w = adc_read();
-        
-        motor_duty_cycle = ((throttle - 1280) / 2808.0);
-        if (motor_duty_cycle > 1.0) {
-            motor_duty_cycle = 1.0;
-        }
-        
-        if (motor_duty_cycle < 0.0) {
-            motor_duty_cycle = 0.0;
-        }
-        
-        
-        if (time_since_last_report > 500000) {
-            //check_rotor_position();
-            //estimate_rotor_position();
-            printf("%3.2f Hz; %3.2f rads-1; S %d; THR %1.2f\n", rotor_phase_frequency, rotor_average_speed, rotor_current_step, motor_duty_cycle);
-            time_since_last_report = 0;
-            
-            //adc_select_input(0);
-            //uint16_t result_u = adc_read();
-            //adc_select_input(1);
-            //uint16_t result_v = adc_read();
-            //adc_select_input(2);
-            //uint16_t result_w = adc_read();
-            
-            //printf("U %d; V %d; W%d;\n", result_u, result_v, result_w);
-        }
-        
-        /*
-        if (time_since_last_report > 500000) {
-            if (!r2sw) {
-                uint8_t rotor_save_step = rotor_current_step;
-                check_rotor_position();
-                
-                adc_select_input(0);
-                uint16_t result_u = adc_read();
-                adc_select_input(1);
-                uint16_t result_v = adc_read();
-                adc_select_input(2);
-                uint16_t result_w = adc_read();
-                
-                printf("Sensed %d; Powered %d; U %d; V %d; W%d;\n", rotor_current_step, rotor_save_step, result_u, result_v, result_w);
-                
-                rotor_current_step = rotor_save_step;
-                
-                r2sw = true;
-            }
-        }
-        
-        if (time_since_last_report > 1000000) {
-            rotor_current_step++;
-            rotor_current_step = rotor_current_step % 6;
-            time_since_last_report = 0;
-            r2sw = false;
-        }
         */
         
         //idle_loop();
